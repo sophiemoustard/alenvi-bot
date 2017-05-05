40f5b6bafcf77afa8d4e272b9027f7c42dc27901
Pierre HOFMAN <pierre.hofman@epitech.eu>
2017-05-05 17:18:11 +0200

    add initial files to test microsoft bot framework

---
+++ app.js
@@ -0,0 +1,85 @@
+const request = require('request');
+const rp = require('request-promise');
+const express = require('express');
+const app = express();
+
+const PORT = "3978";
+
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+app.listen(PORT, function() {
+  console.log('%s listening to port %s', app.name, PORT);
+})
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: "***REMOVED***",
+    appPassword: "***REMOVED***"
+});
+var bot = new builder.UniversalBot(connector);
+app.post('/api/messages', connector.listen());
+
+/*
+** TESTS
+*/
+// rp.post({
+//   uri: "https://login.microsoftonline.com/common/oauth2/v2.0/token",
+//   form: {
+//     grant_type: "client_credentials",
+//     client_id: "***REMOVED***",
+//     client_secret: "***REMOVED***",
+//     scope: "https://graph.microsoft.com/.default"
+//   },
+//   json: true,
+//   // resolveWithFullResponse: true,
+//   // time: true
+// }).then(function (parsedBody) {
+//   console.log(parsedBody);
+//   // console.log("Duration: " + parsedBody.timings.end);
+// }).catch(function (err) {
+//   console.error(err);
+// })
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+var intents = new builder.IntentDialog();
+bot.dialog('/', intents);
+
+intents.matches(/^change name/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your name to %s', session.userData.name);
+    }
+]);
+
+intents.onDefault([
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
+]);
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
