02b33c0abab444d8713752813301e95dd9c95ae3
Pierre HOFMAN <pierre.hofman@epitech.eu>
2017-06-19 11:20:00 +0200

    beautifyied config.js file

--- config.js
+++ config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  'WEBSITE_HOSTNAME': "https://d619f5cc.ngrok.io",
-  'MICROSOFT_APP_ID': "***REMOVED***",
-  'MICROSOFT_APP_PASSWORD': "***REMOVED***",
-  'FACEBOOK_APP_ID': "***REMOVED***",
-  'FACEBOOK_APP_SECRET': "***REMOVED***",
-  'FACEBOOK_PAGE_TOKEN': "EAAN47LGyyhsBAEGfwh2nZBK2JYYVs432ZCVujDY9U1n7RQOClCLlrmJgQmqsj6CJkCg4Fz7L2ANSWguzO3fTZBW0pfMbwX4pZBZC3Sf1RE04luxKp8eEL5Hv6ywKs2DSUbVnKjbZCvBjTIrCC7XCtv12eeu8xZAAAZB6pfZAuD3kkPgZDZD",
-  '***REMOVED***': "***REMOVED***",
-  'TOKEN_SECRET': "Alenvi",
+  WEBSITE_HOSTNAME: 'https://d619f5cc.ngrok.io',
+  MICROSOFT_APP_ID: '***REMOVED***',
+  MICROSOFT_APP_PASSWORD: '***REMOVED***',
+  FACEBOOK_APP_ID: '***REMOVED***',
+  FACEBOOK_APP_SECRET: '***REMOVED***',
+  FACEBOOK_PAGE_TOKEN: 'EAAN47LGyyhsBAEGfwh2nZBK2JYYVs432ZCVujDY9U1n7RQOClCLlrmJgQmqsj6CJkCg4Fz7L2ANSWguzO3fTZBW0pfMbwX4pZBZC3Sf1RE04luxKp8eEL5Hv6ywKs2DSUbVnKjbZCvBjTIrCC7XCtv12eeu8xZAAAZB6pfZAuD3kkPgZDZD',
+  ***REMOVED***: '***REMOVED***',
+  TOKEN_SECRET: 'Alenvi',
   Ogust: {
-    'API_LINK': "https://my.ogust.com/api/v2/apiogust.php?method=",
-    'PUBLIC_KEY': "***REMOVED***",
-    'PRIVATE_KEY': "***REMOVED***"
+    API_LINK: 'https://my.ogust.com/api/v2/apiogust.php?method=',
+    PUBLIC_KEY: '***REMOVED***',
+    PRIVATE_KEY: '***REMOVED***'
   },
   Slack: {
-    'TOKEN': "***REMOVED***",
+    TOKEN: '***REMOVED***',
     channels: {
-      "1b*": "G5QLJ49KL",
-      "2b*": "G5QLHLTPC",
-      "*": "G5QLJ49KL"
+      '1b*': 'G5QLJ49KL',
+      '2b*': 'G5QLHLTPC',
+      '*': 'G5QLJ49KL'
     }
   }
-}
+};
