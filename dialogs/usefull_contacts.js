eb1d4b627b57e1e3d2e6da47a00488f5b603e6e5
Pierre HOFMAN <pierre.hofman@gmail.com>
2017-06-30 14:18:25 +0000

    add usefull_contacts dialog

---
+++ dialogs/usefull_contacts.js
@@ -0,0 +1,17 @@
+const builder = require('botbuilder');
+
+// =========================================================
+// Usefull contacts dialog
+// =========================================================
+
+const showContacts = async (session) => {
+  return session.endDialog(`Médecine du travail:  \n
+🏠 CIAMT Centre Vaugirard  \n
+242 rue de Vaugirard 75015 Paris  \n
+Métro Vaugirard (ligne 12)  \n
+👩 Jessica Silmar  \n
+📞 01 53 53 00 15  \n
+📧 j.silmar@ciamt.org`);
+}
+
+exports.showContacts = [showContacts];
