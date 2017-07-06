d9d91a4bee5471ad6ecc70e2a7801e177eaefce3
Pierre HOFMAN <pierre.hofman@epitech.eu>
2017-07-06 17:33:24 +0200

    Update functions to be more generics, and dialogs in dedicated files

--- app.js
+++ app.js
@@ -99,10 +99,10 @@ bot.on('conversationUpdate', (message) => {
   }
 });
 
-bot.on('error', (e) => {
-  console.error(e);
-  throw new Error(`Error in the bot: ${e}`);
-})
+// bot.on('error', (e) => {
+//   console.error(e);
+//   throw new Error(`Error in the bot: ${e}`);
+// })
 
 // =========================================================
 // Root Dialog
@@ -166,9 +166,8 @@ bot.dialog('/hello_first', require('./dialogs/hello').hello_first);
 bot.dialog('/hello', require('./dialogs/hello').hello);
 
 bot.dialog('/select_show_planning', require('./dialogs/showPlanning').select);
-bot.dialog('/which_person', require('./dialogs/showPlanning').whichPerson);
-bot.dialog('/which_day', require('./dialogs/showPlanning').whichDay);
-bot.dialog('/show_planning', require('./dialogs/showPlanning').showResult);
+bot.dialog('/which_person', require('./dialogs/whichPerson').whichPerson);
+bot.dialog('/which_period', require('./dialogs/whichPeriod').whichPeriod);
 
 bot.dialog('/select_modify_planning', require('./dialogs/modifyPlanning').select);
 bot.dialog('/change_intervention', require('./dialogs/modifyPlanning').changeIntervention);
@@ -186,6 +185,7 @@ bot.dialog('/select_infos', require('./dialogs/infos').select);
 bot.dialog('/hr_docs', require('./dialogs/HRDocs').showHRDocs);
 bot.dialog('/usefull_contacts', require('./dialogs/usefull_contacts').showContacts);
 bot.dialog('/select_pay_sheets', require('./dialogs/pay_sheets').select);
+
 bot.endConversationAction('goodbye', 'Goodbye :)', { matches: /^goodbye/i });
 
 // =========================================================
--- dialogs/showPlanning.js
+++ dialogs/showPlanning.js
@@ -1,11 +1,5 @@
 const builder = require('botbuilder');
 
-const checkOgustToken = require('../helpers/checkOgustToken').checkToken;
-const planning = require('../helpers/planning/treatment.js');
-const { getTeamBySector } = require('../helpers/team');
-const { formatPromptListPersons } = require('../helpers/planning/format');
-const { getCustomers } = require('./../helpers/customers');
-
 // =========================================================
 // Root 'Select show planning' dialog
 // =========================================================
@@ -50,91 +44,6 @@ exports.select = [whichPlanning, redirectToDaySelected];
 // Generic function shared by my own planning and another auxiliary planning
 // =========================================================
 
-// const checkPerson = async (session, args) => {
-//   let targetPlanning = '';
-//   await checkOgustToken(session);
-//   session.sendTyping();
-//   // Because we can recall this dialog with different weeks select, we need to check it
-//   session.dialogData.personChosen = args.personChosen;
-//   session.dialogData.personType = args.personType;
-//
-//   switch (session.dialogData.personChosen) {
-//     case 'Self':
-//       session.replaceDialog();
-//       break;
-//     case 'Auxiliary':
-//     case 'Customer':
-//       targetPlanning = 'son planning';
-//       break;
-//     case 'Community':
-//       targetPlanning = 'le planning de ta communauté';
-//       break;
-//   }
-// }
-
-const whichDay = async (session) => {
-  let days = {};
-  try {
-    await checkOgustToken(session);
-    session.sendTyping();
-    // Because we can recall this dialog with different weeks select, we need to check it
-    if (session.dialogData.weekSelected != 0) {
-      days = planning.getPeriodByOffset(session.dialogData.offset, 'weeks');
-    } else { // If user didn't click on 'Précédent' or 'Suivant', just get current week's days
-      days = planning.getPeriodByOffset(0, 'weeks');
-      session.dialogData.offset = 0;
-    }
-    session.dialogData.days = days;
-    let targetPlanning = '';
-    switch (session.dialogData.personChosen) {
-      case 'Self':
-        targetPlanning = 'ton planning';
-        break;
-      case 'Auxiliary':
-      case 'Customer':
-        targetPlanning = 'son planning';
-        break;
-      case 'Community':
-        targetPlanning = 'le planning de ta communauté';
-        break;
-    }
-    builder.Prompts.choice(session, `Pour quel jour souhaites-tu consulter ${targetPlanning} ?`, days, { maxRetries: 0 });
-  } catch (err) {
-    console.error(err);
-    return session.endDialog("Mince, je n'ai pas réussi à récupérer ton autorisation pour obtenir ces informations :/ Si le problème persiste, essaie de contacter l'équipe technique !");
-  }
-};
-
-const handleWeeksOrGetPlanningSelected = (session, results) => {
-  const params = {};
-  if (results.response) {
-    // Use args to save week's offset in the new dialog => dialogData is unset in each new one
-    params.personChosen = session.dialogData.personChosen;
-    params.personType = session.dialogData.personType;
-    if (results.response.entity === 'Précédent') {
-      params.offset = --session.dialogData.offset;
-      return session.replaceDialog('/show_planning', params);
-    } else if (results.response.entity === 'Suivant') {
-      params.offset = ++session.dialogData.offset;
-      return session.replaceDialog('/show_planning', params);
-    }
-    if (session.dialogData.days[results.response.entity]) {
-      switch (session.dialogData.personType) {
-        case 'Self':
-        case 'Auxiliary':
-        case 'Customer':
-          return planning.getPlanningByChosenDay(session, results);
-        case 'Community':
-          return planning.getCommunityPlanningByChosenDay(session, results);
-      }
-    }
-  } else {
-    return session.cancelDialog(0, '/not_understand');
-  }
-};
-
-exports.showPlanning = [whichPerson, whichDay, handleWeeksOrGetPlanningSelected];
-
 // =========================================================
 // Show another auxiliary planning
 // =========================================================
@@ -154,55 +63,3 @@ exports.showPlanning = [whichPerson, whichDay, handleWeeksOrGetPlanningSelected]
 //     return session.endDialog("Mince, je n'ai pas réussi à récupérer tes collègues :/ Si le problème persiste, essaye de contacter l'équipe technique !");
 //   }
 // };
-
-const whichPerson = async (session, args) => {
-  try {
-    let myRawPersons;
-    let personType;
-    let personPromptMsg;
-    session.dialogData.personType = args.personType || '';
-    await checkOgustToken(session);
-    session.sendTyping();
-    switch (args.personType) {
-      case 'Customer':
-        myRawPersons = await getCustomers(session, session.userData.alenvi.employee_id);
-        personType = 'id_customer';
-        personPromptMsg = 'Quel(le) bénéficiaire précisément ?';
-        break;
-      case 'Auxiliary':
-        myRawPersons = await getTeamBySector(session);
-        personType = 'id_employee';
-        personPromptMsg = 'Quel(le) auxiliaire précisément ?';
-        break;
-      case 'Self':
-      case 'Community':
-        return session.replaceDialog('/which_day', { offset: '0', personChosen: '', personType: session.dialogData.personType });
-      case '':
-        throw new Error('personType is empty');
-      default:
-        break;
-    }
-    session.dialogData.myPersons = await formatPromptListPersons(session, myRawPersons, personType);
-    builder.Prompts.choice(session, personPromptMsg, session.dialogData.myPersons, { maxRetries: 0 });
-  } catch (err) {
-    console.error(err);
-    return session.endDialog("Mince, je n'ai pas réussi à récupérer les personnes correspondantes :/ Si le problème persiste, essaye de contacter l'équipe technique !");
-  }
-};
-
-const redirectToWhichDay = (session, results) => {
-  if (results.response) {
-    if (session.dialogData.myPersons[results.response.entity]) {
-      const params = {
-        offset: 0,
-        personChosen: session.dialogData.myPersons[results.response.entity],
-        personType: session.dialogData.personType
-      };
-      return session.replaceDialog('/which_day', params);
-    }
-  } else {
-    session.cancelDialog(0, '/not_understand');
-  }
-};
-
-exports.showPersonPlanning = [whichPerson, redirectToWhichDay];
