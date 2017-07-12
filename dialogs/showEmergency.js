bff3f229c83723a329c5ce0a9c020a1a5d3e6edc
Jean-Christophe TREBALAG <jean-christophetrebalag@MacBook-Pro-de-Jean-Christophe.local>
2017-07-12 13:46:40 +0200

    add showEmergency dialog

---
+++ dialogs/showEmergency.js
@@ -0,0 +1,26 @@
+const builder = require('botbuilder');
+
+const checkOgustToken = require('../helpers/checkOgustToken').checkToken;
+
+const showEmergency = async (session) => {
+  try {
+    await checkOgustToken(session);
+    session.sendTyping();
+    const msg = new builder.Message(session);
+    msg
+      .attachmentLayout(builder.AttachmentLayout.carousel)
+      .attachments([
+        new builder.HeroCard(session)
+          .title("Situations d'urgence")
+          .buttons([
+            builder.CardAction.openUrl(session, 'https://drive.google.com/open?id=0B3bqjy-Bj6OHN1hzUkh4Zy1WNUk', 'Accéder au PDF')
+          ])
+      ]);
+    session.endDialog(msg);
+  } catch (err) {
+    console.log(err);
+    return session.endDialog("Arf, je n'ai pas réussi à récupérer le document :/ Si le problème persiste, essaie de contacter un administrateur !");
+  }
+};
+
+exports.showEmergency = [showEmergency];
