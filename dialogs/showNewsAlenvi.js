474b324ea6f85ed84d86b138c232e6fc31c2fc42
Jean-Christophe TREBALAG <jean-christophetrebalag@MacBook-Pro-de-Jean-Christophe.local>
2017-07-25 09:42:26 +0200

    Add sendTyping to showNewsAlenvi and remove console.log + some comments

--- dialogs/showNewsAlenvi.js
+++ dialogs/showNewsAlenvi.js
@@ -3,31 +3,18 @@ const builder = require('botbuilder');
 const checkOgustToken = require('../helpers/checkOgustToken').checkToken;
 const { getNewsAlenvi } = require('../helpers/getNewsAlenvi');
 
-// const cards = [];
-
-// const getCardsAttachments = (session, title, shareLink) => {
-//   return cards.push(
-//     new builder.HeroCard(session)
-//       .title(title)
-//       .buttons([
-//         builder.CardAction.openUrl(session, shareLink, 'Visionner')
-//       ])
-//   );
-// };
-
 const showNewsAlenvi = async (session) => {
   try {
     await checkOgustToken(session);
+    session.sendTyping();
     const newsAlenvi = await getNewsAlenvi();
     if (newsAlenvi.length === 0) {
       throw new Error('No files found');
     } else {
       const newsCards = [];
       for (let i = 0, l = newsAlenvi.length; i < l; i++) {
-        // newsCards = await getCardsAttachments(session, newsAlenvi.name, newsAlenvi.webViewLink);
         let link = newsAlenvi[i].webViewLink;
         link = link.replace(/(.+\/view)\?.+/, '$1');
-        console.log(link);
         const card = new builder.HeroCard(session)
           .title(newsAlenvi[i].name)
           .buttons([
