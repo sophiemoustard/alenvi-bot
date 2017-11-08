4b56d360a15c93caf6f8a973ebf11eca895c6eba
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2017-11-08 15:13:36 +0100

    Storing access token in userData

--- helpers/storeUserAddress.js
+++ helpers/storeUserAddress.js
@@ -1,18 +1,11 @@
-const jwt = require('jsonwebtoken');
-
 const users = require('../models/Alenvi/users');
-const { tokenConfig } = require('../config/config');
 
 exports.storeUserAddress = async (session) => {
   try {
     session.userData.alenvi.facebook = session.userData.alenvi.facebook || {};
     session.userData.alenvi.facebook.address = session.message.address;
     const userAddress = session.userData.alenvi.facebook.address;
-    const payload = {
-      _id: session.userData.alenvi._id
-    };
-    const accessToken = jwt.sign(payload, tokenConfig.secret, { expiresIn: tokenConfig.expiresIn });
-    await users.storeUserAddress(payload._id, accessToken, userAddress);
+    await users.storeUserAddress(session.userData.alenvi._id, session.userData.alenvi.token, userAddress);
     console.log('Storing user address in DB...');
   } catch (e) {
     console.error(e.message);
