8772ae4edd5d220f385e46a2165db92f8aa24e1b
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-02-01 18:31:43 +0100

    Add new dialogs to new Directory menu entry

--- dialogs/team.js
+++ dialogs/team.js
@@ -1,31 +1,90 @@
 const builder = require('botbuilder');
 
 const checkOgustToken = require('../helpers/checkOgustToken').checkToken;
-const { getTeamBySector } = require('../helpers/team');
+// const { getTeamBySector } = require('../helpers/team');
+const { getAlenviUsers } = require('../models/Alenvi/users');
+const { getList } = require('../models/Ogust/getList');
+
+const whichDirectory = async (session) => {
+  console.log(session.message);
+  session.sendTyping();
+  await checkOgustToken(session);
+  builder.Prompts.choice(session, 'Quelle section veux-tu consulter ?', 'Mon équipe|Autres communautés|Alenvi-bureau', { maxRetries: 0 });
+};
+
+const redirectToDirectorySelected = (session, results) => {
+  if (results.response) {
+    if (session.userData.alenvi) {
+      switch (results.response.entity) {
+        case 'Mon équipe':
+          session.replaceDialog('/show_team', { team: true });
+          break;
+        case 'Autres communautés':
+          session.replaceDialog('/show_sector_team');
+          break;
+        case 'Alenvi-bureau':
+          session.replaceDialog('/show_team', { staff: true });
+          break;
+      }
+    } else {
+      return session.endDialog('Tu dois te connecter pour accéder à cette fonctionnalité ! :)');
+    }
+  } else {
+    session.cancelDialog(0, '/not_understand');
+  }
+};
+
+exports.selectDirectory = [whichDirectory, redirectToDirectorySelected];
+
+const whichSector = async (session) => {
+  try {
+    session.sendTyping();
+    await checkOgustToken(session);
+    const sectorsRaw = await getList(session.userData.ogust.tokenConfig.token, 'employee.sector');
+    const sectors = sectorsRaw.body.list;
+    const formattedSectors = {};
+    for (const k in sectors) {
+      if (k !== '*' && k !== session.userData.alenvi.sector) {
+        formattedSectors[sectors[k]] = k;
+      }
+    }
+    session.dialogData.sectors = formattedSectors;
+    builder.Prompts.choice(session, 'Quelle communauté précisément ?', formattedSectors, { listStyle: builder.ListStyle.button, maxRetries: 0 });
+  } catch (err) {
+    console.error(err);
+    return session.endDialog("Flûte, impossible de récupérer la liste des communautés pour le moment :/ Réessaie, et si le problème persiste n'hésite pas à contacter l'équipe technique !");
+  }
+};
 
 const formatPerson = async (coworker) => {
   let person = {};
-  if (!coworker.first_name) {
-    person = coworker.last_name;
+  if (!coworker.firstname) {
+    person = coworker.lastname;
   } else {
-    person = `${coworker.first_name} ${coworker.last_name}`;
+    person = `${coworker.firstname} ${coworker.lastname}`;
   }
   return person;
 };
 
-const getCardsAttachments = async (session) => {
+const getCardsAttachments = async (session, params) => {
   const myCards = [];
-  const myRawTeam = await getTeamBySector(session, session.userData.alenvi.sector);
-  const lengthTeam = Object.keys(myRawTeam).length;
-  for (const k in myRawTeam) {
-    if (myRawTeam[k].id_employee == session.userData.alenvi.employee_id && lengthTeam === 1) {
+  // const myRawTeam = await getTeamBySector(session, session.userData.alenvi.sector);
+  const myRawTeam = await getAlenviUsers(session.userData.alenvi.token, params);
+  const myTeam = myRawTeam.body.data.users;
+  const lengthTeam = myTeam.length;
+  for (let i = 0; i < lengthTeam; i++) {
+    if (myTeam[i].employee_id == session.userData.alenvi.employee_id && lengthTeam === 1) {
       return session.endDialog('Il semble que tu sois le premier membre de ta communauté ! :)');
     }
-    if (myRawTeam[k].id_employee != session.userData.alenvi.employee_id) {
-      const person = await formatPerson(myRawTeam[k]);
-      const mobilePhone = myRawTeam[k].mobile_phone || 'N/A';
-      const contact = `https://m.me/${myRawTeam[k].skype_id}`;
-      const picture = myRawTeam[k].picture || 'https://cdn.head-fi.org/g/2283245_l.jpg';
+    if (myTeam[i].employee_id != session.userData.alenvi.employee_id) {
+      const person = await formatPerson(myTeam[i]);
+      const mobilePhone = myTeam[i].mobilePhone || 'N/A';
+      const contact = myTeam[i].facebook && myTeam[i].facebook.address ? `https://m.me/${myTeam[i].facebook.address.user.id}` : null;
+      const picture = myTeam[i].picture || 'https://cdn.head-fi.org/g/2283245_l.jpg';
+      const buttons = [];
+      if (contact) {
+        buttons.push(builder.CardAction.openUrl(session, contact, 'Contacter'));
+      }
       myCards.push(
         new builder.ThumbnailCard(session)
           .title(person)
@@ -33,20 +92,32 @@ const getCardsAttachments = async (session) => {
           .images([
             builder.CardImage.create(session, picture)
           ])
-          .buttons([
-            builder.CardAction.openUrl(session, contact, 'Contacter')
-          ])
+          .buttons(buttons)
       );
     }
   }
   return myCards;
 };
 
-const showMyTeam = async (session) => {
+const showMyTeam = async (session, results) => {
   try {
     session.sendTyping();
     await checkOgustToken(session);
-    const cards = await getCardsAttachments(session);
+    const queryParams = {};
+    console.log('RESULTS', results);
+    if (results.response) {
+      if (session.dialogData.sectors) {
+        queryParams.sector = session.dialogData.sectors[results.response.entity];
+        queryParams.role = 'Auxiliaire';
+      }
+    } else if (results.staff) {
+      queryParams.sector = '*';
+    } else if (results.team) {
+      queryParams.sector = session.userData.alenvi.sector;
+    } else {
+      session.cancelDialog(0, '/not_understand');
+    }
+    const cards = await getCardsAttachments(session, queryParams);
     const message = new builder.Message(session)
       .attachmentLayout(builder.AttachmentLayout.carousel)
       .attachments(cards);
@@ -58,3 +129,4 @@ const showMyTeam = async (session) => {
 };
 
 exports.showTeam = [showMyTeam];
+exports.showSectorTeam = [whichSector, showMyTeam];
