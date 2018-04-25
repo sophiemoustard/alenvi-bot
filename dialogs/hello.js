df6b2777ad2b66a37c40b6e1fa34798b6e7d3b12
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-04-25 13:48:38 +0200

    Add laterjs + reminder in hello dialog

--- dialogs/hello.js
+++ dialogs/hello.js
@@ -3,6 +3,7 @@
 // =========================================================
 
 const builder = require('botbuilder');
+const later = require('later');
 // const moment = require('moment');
 // const BotMetrics = require('botmetrics');
 // const { sendEndorsementToSlack } = require('../helpers/sendEndorsement');
@@ -39,10 +40,10 @@ exports.hello_first = [
 // };
 
 const getEndSignupCardAttachment = (session) => {
-  const uri = `${process.env.WEBSITE_HOSTNAME}/signupComplete?id=${session.userData.alenvi._id}&token=${session.userData.alenvi.token}`;
+  const uri = `${process.env.WEBSITE_HOSTNAME}/optionalDocuments`;
   return new builder.HeroCard(session)
-    .title('Terminer inscription')
-    .text('Merci de bien vouloir terminer ton inscription ! :)')
+    .title('Envoi documents optionels')
+    .text('Merci de bien vouloir cliquer sur le lien pour envoyer tes justificatifs (facture téléphone, navigo, attestation sécu,...) :)')
     .images([
       builder.CardImage.create(session, 'https://res.cloudinary.com/alenvi/image/upload/v1499948101/images/bot/Pigi.png')
     ])
@@ -51,7 +52,7 @@ const getEndSignupCardAttachment = (session) => {
     ]);
 };
 
-const showEndSignupCard = (session) => {
+const showOptionalDocsCard = (session) => {
   session.sendTyping();
   const card = getEndSignupCardAttachment(session);
   const message = new builder.Message(session).addAttachment(card);
@@ -61,17 +62,40 @@ const showEndSignupCard = (session) => {
 const rootGreetingMenu = async (session) => {
   session.sendTyping(); // Hello ${session.userData.alenvi.firstname}!
   // whichCommunity(session, session.userData.alenvi.role, session.userData.alenvi.sector);
-  if (session.message.sourceEvent.referral && session.message.sourceEvent.referral.ref === 'signup_complete') {
+  // if (session.message.sourceEvent.referral && session.message.sourceEvent.referral.ref === 'signup_complete') {
+  //   await checkToken(session);
+  //   session.send("Merci d'avoir completé ton inscription ! :)");
+  // }
+  let reminder = null;
+  if (session.userData.alenvi.administrative) {
+    if ((session.userData.alenvi.administrative.navigoInvoice && session.userData.alenvi.administrative.navigoInvoice.has && !session.userData.alenvi.administrative.navigoInvoice.link)
+      || (session.userData.alenvi.administrative.mutualFund && session.userData.alenvi.administrative.mutualFund.has && !session.userData.alenvi.administrative.mutualFund.link)
+      || (session.userData.alenvi.administrative.phoneInvoice && session.userData.alenvi.administrative.phoneInvoice.has && !session.userData.alenvi.administrative.phoneInvoice.link)
+      || (session.userData.alenvi.administrative.certificates && session.userData.alenvi.administrative.certificates.has && session.userData.alenvi.administrative.certificates.docs.length === 0)) {
+      if (!session.userData.reminderSet) {
+        later.date.localTime();
+        const sched = later.parse.text('at 13:50');
+        reminder = later.setInterval(async () => {
+          await checkToken(session);
+          showOptionalDocsCard(session);
+        }, sched);
+        session.userData.reminderSet = true;
+      }
+    }
+  }
+  if (session.message.sourceEvent.referral && session.message.sourceEvent.referral.ref === 'optional_docs_complete') {
+    session.sendTyping();
     await checkToken(session);
+    reminder.clear();
     session.send("Merci d'avoir completé ton inscription ! :)");
   }
   // if (moment(session.userData.alenvi.createdAt).add('45', 'days').isSame(moment(), 'day') && session.userData.alenvi.administrative && !session.userData.alenvi.administrative.endorsement) {
   //   await sendEndorsementToSlack(session);
   // }
-  if (session.userData.alenvi.administrative && !session.userData.alenvi.administrative.signup.complete) {
-    await checkToken(session);
-    showEndSignupCard(session);
-  }
+  //  if (session.userData.alenvi.administrative && !session.userData.alenvi.administrative.signup.complete) {
+  //    await checkToken(session);
+  //    showEndSignupCard(session);
+  //  }
   builder.Prompts.choice(session, 'Comment puis-je t’aider ? 😉', 'Consulter planning|Modifier planning|Bénéficiaires|Répertoire|Infos|Formation|URGENCE', { maxRetries: 0 });
   // if (session.userData.alenvi.role == 'admin' || session.userData.alenvi.role == 'coach') {
   //   builder.Prompts.choice(session, 'Comment puis-je t’aider ? 😉', 'Consulter planning|Modifier planning|Bénéficiaires|Répertoire|Infos|Formation|URGENCE|Accueil aux.', { maxRetries: 0 });
