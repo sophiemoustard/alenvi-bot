42bcd0ce534dc70e0c2246f9f23342d8f9087771
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-05-29 15:46:00 +0200

    Fix when no property

--- helpers/checkOptionalDocs.js
+++ helpers/checkOptionalDocs.js
@@ -1,6 +1,9 @@
 exports.checkOptionalDocs = (adminObj) => {
   const optionalDocs = ['navigoInvoice', 'mutualFund', 'phoneInvoice', 'certificates'];
   return optionalDocs.every((value) => {
+    if (!adminObj[value]) {
+      return true;
+    }
     if (adminObj[value] && adminObj[value].has && value !== 'certificates') {
       return !!adminObj[value].link;
     }
