f3afd2ba165e1ae456816b9c7a7206954b022783
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-05-29 15:34:45 +0200

    Add checkOptionalDocs helper + use it in hello dialog

---
+++ helpers/checkOptionalDocs.js
@@ -0,0 +1,14 @@
+exports.checkOptionalDocs = (adminObj) => {
+  const optionalDocs = ['navigoInvoice', 'mutualFund', 'phoneInvoice', 'certificates'];
+  return optionalDocs.every((value) => {
+    if (adminObj[value] && adminObj[value].has && value !== 'certificates') {
+      return !!adminObj[value].link;
+    }
+    if (adminObj[value] && adminObj[value].has && value === 'certificates') {
+      return adminObj[value].docs.length > 0;
+    }
+    if (adminObj[value] && !adminObj[value].has) {
+      return true;
+    }
+  });
+};
