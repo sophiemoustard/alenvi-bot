5ebfde655b1a93b9f6930f3fa26c3400433cec69
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-07-03 00:53:38 +0200

    Replace request to reflects API migration changes

--- helpers/storeUserAddress.js
+++ helpers/storeUserAddress.js
@@ -4,9 +4,9 @@ exports.storeUserAddress = async (session) => {
   try {
     session.userData.alenvi.facebook = session.userData.alenvi.facebook || {};
     session.userData.alenvi.facebook.address = session.message.address;
-    const userAddress = session.userData.alenvi.facebook.address;
+    const userAddress = { facebook: session.userData.alenvi.facebook };
     console.log('Storing user address in DB...');
-    await users.storeUserAddress(session.userData.alenvi._id, session.userData.alenvi.token, userAddress);
+    await users.updateAlenviUserById(session.userData.alenvi._id, session.userData.alenvi.token, userAddress);
   } catch (e) {
     console.error(e.message);
   }
