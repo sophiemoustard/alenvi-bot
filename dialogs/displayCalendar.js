858aed2ae006263f0a0a79f60132cae014e46870
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-10-10 14:11:03 +0200

    Modify planning links to use webapp instead of webview

--- dialogs/displayCalendar.js
+++ dialogs/displayCalendar.js
@@ -1,42 +1,35 @@
 const builder = require('botbuilder');
-const _ = require('lodash');
+// const _ = require('lodash');
 
-const { getCustomers } = require('../models/Ogust/employees');
+// const { getCustomers } = require('../models/Ogust/employees');
 
 const checkOgustToken = require('../helpers/checkOgustToken').checkToken;
 
-const getFirstEmployeeCustomer = async (session) => {
-  try {
-    const myCustomersRaw = await getCustomers(session.userData.ogust.tokenConfig.token, session.userData.alenvi.employee_id);
-    const myCustomers = myCustomersRaw.body.data.customers.filter(customer => !customer.last_name.match(/^alenvi/i));
-    const firstEmployeeCustomer = _.sortBy(myCustomers, ['last_name']).slice(0, 1);
-    return firstEmployeeCustomer;
-  } catch (e) {
-    console.error(e);
-    return session.endDialog("Mince, je n'ai pas réussi à récupérer tes bénéficiaires");
-  }
-};
+// const getFirstEmployeeCustomer = async (session) => {
+//   try {
+//     const myCustomersRaw = await getCustomers(session.userData.ogust.tokenConfig.token, session.userData.alenvi.employee_id);
+//     const myCustomers = myCustomersRaw.body.data.customers.filter(customer => !customer.last_name.match(/^alenvi/i));
+//     const firstEmployeeCustomer = _.sortBy(myCustomers, ['last_name']).slice(0, 1);
+//     return firstEmployeeCustomer;
+//   } catch (e) {
+//     console.error(e);
+//     return session.endDialog("Mince, je n'ai pas réussi à récupérer tes bénéficiaires");
+//   }
+// };
 
 const getCardsAttachments = async (session, args) => {
   if (!args) {
     throw new Error('No personType and/or personChosen');
   }
   const title = 'Consulter planning';
-  const { sector } = session.userData.alenvi;
-  let employeeId = '';
-  let customerId = '';
+  const { _id } = session.userData.alenvi;
   let url = '';
-  let customer;
   switch (args.personType) {
     case 'Auxiliary':
-      employeeId = session.userData.alenvi.employee_id;
-      url = `${process.env.WEBSITE_HOSTNAME}/bot/calendar?id_employee=${employeeId}&access_token=${session.userData.alenvi.token}`;
+      url = `${process.env.WEBSITE_HOSTNAME}/ni/${_id}/planning?auxiliary=true&self=true`;
       break;
     case 'Customer':
-      customer = await getFirstEmployeeCustomer(session);
-      // customerId = args.personChosen.customer_id;
-      customerId = customer[0].id_customer;
-      url = `${process.env.WEBSITE_HOSTNAME}/bot/calendar?id_customer=${customerId}&sector=${sector}&access_token=${session.userData.alenvi.token}`;
+      url = `${process.env.WEBSITE_HOSTNAME}/ni/${_id}/planning?customer=true`;
   }
   const myCards = [];
   myCards.push(
