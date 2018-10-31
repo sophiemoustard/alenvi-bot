7286c822de825c468fbf568ea1f984edb51da950
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-10-31 14:18:20 +0100

    Update useful contacts info

--- dialogs/usefull_contacts.js
+++ dialogs/usefull_contacts.js
@@ -1,17 +1,13 @@
-const builder = require('botbuilder');
-
 // =========================================================
 // Usefull contacts dialog
 // =========================================================
 
-const showContacts = (session) => {
-  return session.endDialog(`Médecine du travail:  \n
-CIAMT Centre Vaugirard  \n
-242 rue de Vaugirard 75015 Paris  \n
-Métro Vaugirard (ligne 12)  \n
-👩 Jessica Silmar  \n
-📞 01 53 53 00 15  \n
-📧 j.silmar@ciamt.org`);
-}
+const showContacts = session => session.endDialog(`Médecine du travail:  \n
+CIAMT  \n
+1 Ter rue Balzac  \n
+75008 Paris  \n
+👩 Aminata BA  \n
+📞 01 53 53 00 13  \n
+📧 a.ba@ciamt.org`);
 
 exports.showContacts = [showContacts];
